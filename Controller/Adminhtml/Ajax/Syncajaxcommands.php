4d88ad8defe246df4125c8c799ea2367c7898824
jose.luis@saleslayer.com <jose.luis@saleslayer.com>
2023-11-08 12:42:44 +0100

    [MAG-181] - fix: duplicated property delete

--- Controller/Adminhtml/Ajax/Syncajaxcommands.php
+++ Controller/Adminhtml/Ajax/Syncajaxcommands.php
@@ -11,7 +11,6 @@ class Syncajaxcommands extends \Magento\Framework\App\Action\Action
     /**
      * @var \Magento\Framework\UrlInterface
      */
-    protected $resultFactory;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
